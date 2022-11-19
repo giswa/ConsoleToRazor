58e08c73614bcdc141eaf66c5f5359fcc1459736
gislain wautriche <giswautriche@gmail.com>
2022-11-19 23:00:32 +0100

    minimal implementation of a host using Kestrel

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 
 namespace myApp
 {
@@ -7,9 +9,21 @@ namespace myApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
 
-            var host = new WebHostBuilder();
+           // minimal implementation of a host
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                // Configuring the minimal app ( always return simple "hello world" without http header)
+                .Configure(app => {
+                    app.Run(context => context.Response.WriteAsync("Hello World!"));
+                }) ;
+            
+            Console.WriteLine("Host running");
+            
+            host.Build().Run();
+            
+
         }
 
     }
